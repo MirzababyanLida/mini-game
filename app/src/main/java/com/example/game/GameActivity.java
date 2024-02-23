2f6a9670da796f9b3a46949a633eda0d9a1c12e9
MirzababyanLida <lidamirzababyan@gmail.com>
2024-02-23 21:13:33 +0400

    mini game

---
+++ app/src/main/java/com/example/game/GameActivity.java
@@ -0,0 +1,17 @@
+package com.example.game;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    GameView gameView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+}
